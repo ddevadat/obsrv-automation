31bc01feed9999bdbd7089cef15ae1638e14e742
Deepak Devadathan <deepak.devadathan@oracle.com>
2023-09-04 08:07:09 +0000

    created svc gw
    Signed-off-by: Deepak Devadathan <deepak.devadathan@oracle.com>

--- terraform/modules/oci/vcn/main.tf
+++ terraform/modules/oci/vcn/main.tf
@@ -6,11 +6,52 @@ terraform {
   }
 }
 
-######################################################################
-#                      Create VCN                                    # 
-######################################################################
+# -----------------------------------------------------------------------------
+# Create VCN
+# -----------------------------------------------------------------------------
 resource "oci_core_vcn" "vcn" {
   cidr_blocks    = var.vcn_cidr
   compartment_id = var.compartment_id
   display_name   = "${var.building_block}-${var.env}-vcn"
+}
+
+
+# -----------------------------------------------------------------------------
+# Create NAT Gateway
+# -----------------------------------------------------------------------------
+resource "oci_core_nat_gateway" "nat-gw" {
+  compartment_id = var.compartment_id
+  vcn_id = oci_core_vcn.vcn.id
+  display_name = "${var.building_block}-${var.env}-nat-gw"
+}
+
+# -----------------------------------------------------------------------------
+# Create Internet Gateway
+# -----------------------------------------------------------------------------
+resource "oci_core_internet_gateway" "int-gw" {
+  compartment_id = var.compartment_id
+  vcn_id         = oci_core_vcn.vcn.id
+  display_name   = "${var.building_block}-${var.env}-int-gw"
+}
+
+# -----------------------------------------------------------------------------
+# Create Service Gateway
+# -----------------------------------------------------------------------------
+
+data "oci_core_services" "service_gateway_all_oci_services" {
+  filter {
+    name   = "name"
+    values = ["All .* Services In Oracle Services Network"]
+    regex  = true
+  }
+}
+
+
+resource "oci_core_service_gateway" "svc-gw" {
+  compartment_id = var.compartment_id
+  vcn_id          = oci_core_vcn.vcn.id
+  services {
+        service_id = lookup(data.oci_core_services.service_gateway_all_oci_services.services[0], "id")
+  }
+  display_name = "${var.building_block}-${var.env}-svc-gw"
 }
